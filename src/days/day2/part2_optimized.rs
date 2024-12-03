b76d7ee5cce39a82df48b66a3df912385072d542
Manoj Mahalingam <manojlds@gmail.com>
2024-12-03 01:58:42 +0000

    Using ReportState enum

--- src/days/day2/part2_optimized.rs
+++ src/days/day2/part2_optimized.rs
@@ -1,27 +1,32 @@
 use std::fs;
 
+#[derive(PartialEq)]
+enum ReportState {
+    Unknown,
+    Descending,
+    Ascending,
+}
+
 fn is_safe(report: Vec<i32>) -> bool {
     if report.len() < 2 {
         return true;
     }
 
     let mut violations = 0;
-    let mut trend = None;
+    let mut trend = ReportState::Unknown;
 
     for pair in report.windows(2) {
         let diff = pair[1] - pair[0];
-        let current_trend = if diff > 0 { "ascending" } else { "descending" };
+        let current_trend = if diff > 0 { ReportState::Ascending } else { ReportState::Descending };
 
-        if trend == None {
-            trend = Some(current_trend);
-        } else if let Some(existing_trend) = trend {
-            if existing_trend != current_trend {
+        if trend == ReportState::Unknown {
+            trend = current_trend;
+        } else if trend != current_trend {
                 violations += 1;
                 if violations > 1 {
                     return false;
                 }
                 continue;
-            }
         }
 
         if diff.abs() > 3 || diff == 0 {
